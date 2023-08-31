ebb223bc50cf8663e7b45c1d4735e2e2f4462f2e
JEGANEESHWARAN <js1088457@gmail.com>
2023-08-31 23:49:48 +0530

    Arithmetic_operator

---
+++ operator.js
@@ -0,0 +1,10 @@
+let Firstnumber=78;
+let SecondNumber=45;
+let result=Firstnumber+SecondNumber;
+let result1=Firstnumber-SecondNumber;
+let result2=Firstnumber*SecondNumber;
+let result3=Firstnumber/SecondNumber;
+console.log("the addition of two number = "+result);
+console.log("the addition of two number = "+result1);
+console.log("the addition of two number = "+result2);
+console.log("the addition of two number = "+result3);
