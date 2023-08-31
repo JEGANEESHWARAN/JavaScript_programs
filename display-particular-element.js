5a489be1c241f79ec6972bb41c8ea2a8cec54064
JEGANEESHWARAN <js1088457@gmail.com>
2023-08-31 23:59:12 +0530

    Displayind the particular element in the given list

---
+++ display-particular-element.js
@@ -0,0 +1,11 @@
+let person={
+    nam:"jegan",
+    age:20,
+    city:"chennai",
+    aim:"software engineer",
+    gender:"male",
+    isAlive:true
+
+}
+console.log(person.nam);
+console.log(person['isAlive']);
