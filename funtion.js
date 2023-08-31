15110abde398389715d65657a3696438042ec8df
JEGANEESHWARAN <js1088457@gmail.com>
2023-08-31 23:56:24 +0530

    Funtion_calling

---
+++ funtion.js
@@ -0,0 +1,12 @@
+function add(num1,num2)
+{
+let result=num1+num2;
+console.log("the addition of two number = "+result);
+}
+add(23,45)
+function name(fname,sname){
+    let msg="hello"+ fname +"good" + sname;
+    console.log(msg);
+}
+name(" i am  ","  person")
+name(" i am not  "," person")
