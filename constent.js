ec1c655739a132d2883e76f57938b0bd07e1ec10
JEGANEESHWARAN <js1088457@gmail.com>
2023-09-06 23:16:04 +0530

    Constants

---
+++ constent.js
@@ -0,0 +1,18 @@
+// program to include constants
+
+const a = 5;
+console.log(a);
+
+// constants are block-scoped
+{
+    const a = 50;
+    console.log(a);
+}
+console.log(a);
+
+const arr = ['work', 'exercise', 'eat'];
+console.log(arr);
+
+// add elements to arr array
+arr[3] = 'hello';
+console.log(arr);
