4d14119b90c59cb1b0900c362e6e76a1d0ec388d
JEGANEESHWARAN <js1088457@gmail.com>
2023-09-06 23:17:17 +0530

    Perform_function_overloading

---
+++ Overloding.js
@@ -0,0 +1,25 @@
+// program to perform function overloading
+
+function sum() {  
+    
+    // if no argument        
+    if (arguments.length == 0) {  
+        console.log('You have not passed any argument');  
+    }
+
+    // if only one argument 
+    else if (arguments.length == 1) {  
+        console.log('Pass at least two arguments');  
+    }
+
+    // multiple arguments
+    else {
+        let result = 0;
+        let length = arguments.length;
+    
+        for (i = 0; i < length; i++) {  
+            result = result + arguments[i];  
+        }  
+        console.log(result); 
+    }  
+}
